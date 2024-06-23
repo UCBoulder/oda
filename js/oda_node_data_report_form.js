6cd81d6879080547c06a63e176c06d511b39b4f9
protitude <miles.france@gmail.com>
2024-06-23 16:22:40 -0600

    js: fix select2 - install: add terms

--- js/oda_node_data_report_form.js
+++ js/oda_node_data_report_form.js
@@ -1,13 +1,13 @@
 (function ($) {
   $(document).ready(function () {
     $('#edit-field-oda-population').select2();
-    $('#edit-field-level-of-detail').select2();
+    $('#edit-field-oda-level-of-detail').select2();
     $('#edit-field-oda-cu-stats').select2();
     $('#edit-field-oda-course-data').select2();
     $('#edit-field-oda-term').select2();
     $('#edit-field-oda-type').select2();
     $('#edit-field-oda-academic-terms-covered').select2();
-    $('#edit-field-demographics').select2();
+    $('#edit-field-oda-demographics').select2();
     $('#edit-field-oda-peers-ext-org').select2();
   });
 }(jQuery));
