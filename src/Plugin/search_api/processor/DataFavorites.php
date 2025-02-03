f1592e4d48791b7425f50dcdc54239940b3319ae
protitude <miles.france@gmail.com>
2025-02-03 16:55:47 -0700

    DataFavorites #827
    code fix

--- src/Plugin/search_api/processor/DataFavorites.php
+++ src/Plugin/search_api/processor/DataFavorites.php
@@ -52,8 +52,8 @@ class DataFavorites extends ProcessorPluginBase {
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new self(
       $configuration,
       $plugin_id,
       $plugin_definition,
