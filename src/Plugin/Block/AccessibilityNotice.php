b346183617548b97b4fba587c07cf9e0ec6fbeff
protitude <miles.france@gmail.com>
2025-09-24 12:17:37 -0400

    phpcs fixes UCBoulder/oit_dingo#1033

--- src/Plugin/Block/AccessibilityNotice.php
+++ src/Plugin/Block/AccessibilityNotice.php
@@ -5,12 +5,12 @@ namespace Drupal\oda\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\oit\Plugin\BlockUuidQuery;
-use Drupal\views\Views;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * oda Accessibility Notice.
+ * Oda Accessibility Notice.
  *
  * @Block(
  *   id = "oda_accessibility_notice",
@@ -35,6 +35,13 @@ class AccessibilityNotice extends BlockBase implements
   protected $entityInterface;
 
   /**
+   * The route match service.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
    * {@inheritdoc}
    *
    * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
@@ -55,6 +62,7 @@ class AccessibilityNotice extends BlockBase implements
       $plugin_definition,
       $container->get('entity_type.manager'),
       $container->get('oit.block.uuid.query'),
+      $container->get('current_route_match'),
     );
   }
 
@@ -71,6 +79,8 @@ class AccessibilityNotice extends BlockBase implements
    *   Invokes renderer.
    * @param \Drupal\oit\Plugin\BlockUuidQuery $block_uuid_query
    *   Loads block.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match service.
    */
   public function __construct(
     array $configuration,
@@ -78,24 +88,27 @@ class AccessibilityNotice extends BlockBase implements
     $plugin_definition,
     EntityTypeManagerInterface $entity_interface,
     BlockUuidQuery $block_uuid_query,
+    RouteMatchInterface $route_match,
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityInterface = $entity_interface;
     $this->blockUuidQuery = $block_uuid_query;
+    $this->routeMatch = $route_match;
   }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $node = \Drupal::routeMatch()->getParameter('node');
+    $node = $this->routeMatch->getParameter('node');
     $type = $node ? $node->get('field_oda_type')->getValue()[0]['target_id'] : NULL;
 
     if ($type == 1129) {
       $render_query = $this->blockUuidQuery;
       $render_query->getBidByUuid('1a9a7a82-bd59-4078-90d4-2b410959fac5');
       $render = $render_query->loadBlock();
-    } else {
+    }
+    else {
       $render = '';
     }
     return [
